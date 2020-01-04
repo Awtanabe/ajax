8b5d59e425dfcc59c1adbaa504dffbe76cbfa3a9
Awtanabe <panser098@gmail.com>
2020-01-04 12:30:55 +0900

    todo 投稿機能

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,8 +3,20 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
   end
 
   def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save!
+      redirect_to todos_path
+    else
+      redirect_to new_todo_path
+    end
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title)
   end
 end
