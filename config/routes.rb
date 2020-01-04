24bbc9e6c5a978823e8b0cb949a49d6bc481dbab
Awtanabe <panser098@gmail.com>
2020-01-04 12:23:21 +0900

    todo controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'todos/index'
+  get 'todos/new'
+  get 'todos/create'
   root "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
